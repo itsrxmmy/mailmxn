ac7aa77c9335196238ac032a2709c6acb90667cb
Rammy Aly <ramaly544@gmail.com>
2021-08-04 20:15:47 -0700

    v1.1.8 update

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 '''
 Project Name: mailmxn
+Project Version Tag: v1.1.8 - Beta
 Author: itsrxmmy
 Description: Email Automation Tool
 '''
 
-coming_soon = "\n\n\nComing Soon."
+coming_soon = "\nComing Soon."
 error = "\nAn error occurred."
-pip_upgrade = "pip install --upgrade pip"
-pip_requirements = "pip install -r requirements.txt"
+pip_upgrade = "python -m pip install --upgrade pip"
+pip_requirements = "python -m pip install -r requirements.txt"
 
 def on_start():
     try:
@@ -27,6 +28,7 @@ import datetime
 import smtplib, ssl
 import json
 import random
+import threading
 from os import system, name
 
 class clr:
@@ -78,13 +80,14 @@ banner = """
  ░███ ░███ ░███  ███░░███  ░███  ░███  ░███ ░███ ░███   ███░░░███  ░███ ░███
  █████░███ █████░░████████ █████ █████ █████░███ █████ █████ █████ ████ █████
 ░░░░░ ░░░ ░░░░░  ░░░░░░░░ ░░░░░ ░░░░░ ░░░░░ ░░░ ░░░░░ ░░░░░ ░░░░░ ░░░░ ░░░░░
-
 """
 
 description = """
-Version 1.0.0 (Beta)
+
+Version 1.1.8 - Beta
 Developed by @itsrxmmy (https://www.github.com/itsrxmmy/)
 Official Repository: https://www.github.com/itsrxmmy/mailmxn/
+License: MIT License - Copyright (c) 2021 Rammy Aly
 
 """
 
@@ -99,12 +102,15 @@ choices = """
 
 [01] Email Flooder                                            [02] Email Automation (Coming Soon)
 [03] Email Account Generator (Coming Soon)                    [04] License
-[05] End Session
+[05] Tutorial (Coming Soon)                                   [06] End Session
 
 """
 
 def _return():
-    print("\n\nPress enter to return to the Menu.    ", input())
+    input("\n\nPress enter to return to the Menu.    ")
+    main()
+
+def force_return():
     main()
 
 def main():
@@ -112,72 +118,128 @@ def main():
     print(banner)
     print(description)
     print(choices)
+    trash = 0
     while True:
-        choice = input("> Choice: ")
-        if choice == "01" or "1":
+        choice = str(input("> Choice: "))
+        if choice == "01" or choice == "1":
             mail_flooder()
             break
-        elif choice == "02" or "2":
+        elif choice == "02" or choice == "2":
             print(coming_soon)
             _return()
             break
-        elif choice == "03" or "3":
+        elif choice == "03" or choice == "3":
             print(coming_soon)
             _return()
             break
-        elif choice == "04" or "4":
+        elif choice == "04" or choice == "4":
             LICENSE()
             _return()
             break
-        elif choice == "05" or "5":
+        elif choice == "05" or choice == "5":
+            print(coming_soon)
+            _return()
+            break
+        elif choice == "06" or choice == "6":
             end_session()
             break
         else:
-            continue
+            trash += 1
+            if trash == 7:
+                force_return()
+                break
+            else:
+                continue
+        break
 
 
 def mail_flooder():
     print()
     print()
-    sender_email = input("<str> Sender Email: ")
-    sender_password = input("<str> Sender Password: ")
     while True:
-        try:
-            amount_of_targets = int(input("<int> Amount of Targets: "))
-        except:
-            print("Must be an Integer.")
+        sender_email = str(input("<str> Sender Email: "))
+        if "@" not in sender_email and "." not in sender_email:
+            if sender_email == "cancel" or sender_email == "//":
+                force_return()
+                break
+            else:
+                print("Must be a valid email address.")
+                continue
+        elif "@" in sender_email and "." in sender_email:
+            break
+        else:
+            print("Must be a valid email address.")
             continue
+        break
+    while True:
+        sender_password = str(input("<str> Sender Password: "))
+        if len(sender_password) == 0:
+            print("Password is required to log in.")
+            continue
+        elif len(sender_password) < 6:
+            if sender_password == "cancel" or sender_password == "//":
+                force_return()
+                break
+            else:
+                print("Password is only <{0}> characters long. Are you sure you've entered it properly?".format(str(len(sender_password))))
+                continue
         else:
             break
+        break
+    while True:
+        amount_of_targets = input("<int> Amount of Targets: ")
+        if amount_of_targets == "cancel" or amount_of_targets == "//":
+            force_return()
+            break
+        else:
+            try:
+                amount_of_targets = int()
+            except:
+                print("Must be an integer.")
+                continue
+            else:
+                break
         break
     target_emails = []
-    for i in range(amount_of_targets):
-        target_email = input("<str> Target Email: ")
-        target_emails.append(target_email)
-    while True:
-        try:
-            amount_of_messages = int(input("<int> Amount of Messages: "))
-        except:
-            print("Must be an Integer.")
-            continue
-        else:
+    for i in range(amount_of_targets):  
+        target_email = str(input("<str> Target Email: "))
+        if target_email == "cancel" or target_email == "//":
+            force_return()
             break
-        break
-    message = input("<str> Message: ")
-    try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
-    except:
-        print(error)
-    else:
-        for i in range(amount_of_messages):
+        else:
+            target_emails.append(target_email)
+    while True:
+        amount_of_messages = input("<int> Amount of Targets: ")
+        if amount_of_messages == "cancel" or amount_of_messages == "//":
+            force_return()
+            break
+        else:
             try:
-                server.sendmail(sender_email, target_emails, message)
-                print("(1) Message has been sent.")
+                amount_of_messages = int()
             except:
-                print(error)
+                print("Must be an integer.")
                 continue
+            else:
+                break
+        break
+    message = str(input("<str> Message: "))
+    if message == "cancel" or message == "//":
+        force_return()
+    else:
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(sender_email, sender_password)
+        except:
+            print(error)
+        else:
+            for i in range(amount_of_messages):
+                try:
+                    server.sendmail(sender_email, target_emails, message)
+                    print("<1> Message has been sent.")
+                except:
+                    print("Failed to send <1> Message.")
+                    continue
     _return()
 
 def LICENSE():
@@ -188,9 +250,18 @@ def LICENSE():
     print()
 
 def end_session():
-    clear()
-    print("\nSession Ended.")
-
+    while True:
+        confirmation = str(input("> Are you sure you would like to end this session? [Y/n]: "))
+        if confirmation == "Y" or confirmation == "y":
+            clear()
+            print("\nSession Ended.")
+            break
+        elif confirmation == "N" or confirmation == "n":
+            force_return()
+            break
+        else:
+            continue
+        break
 
 main()
 
